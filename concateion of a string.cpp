bb4b500ee81ad1f96acaa1b4a1bccc9b567a8cca
aryan42bit <aryankulkarni6969@gmail.com>
2024-08-26 22:03:37 +0530

    Create concateion of a string.cpp

---
+++ concateion of a string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Aryan";
+    last_name = "Kulkarni";
+
+    cout<<first_name+last_name;
+}
+
+/*
+AryanKulkarni
+*/
