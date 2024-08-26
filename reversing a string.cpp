327f5f3f1d4fd6ee81e09a5b0d1df263066a67e2
aryan42bit <aryankulkarni6969@gmail.com>
2024-08-26 22:04:56 +0530

    Create reversing a string.cpp

---
+++ reversing a string.cpp
@@ -0,0 +1,20 @@
+//aryankulkarni//
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cout<<"Enter a string: ";
+    cin>>a;
+    int n = a.length();
+    
+    for(int i=n-1;i>=0;i--)
+    {
+        cout<<a[i];
+    }
+}
+
+/*
+Enter a string: Messi
+isseM
+*/
